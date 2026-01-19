66e30422fa23e2e53fdd2db6715ed83d02186753
Eyal Amir <eyalamirmusic@gmail.com>
2026-01-18 18:21:41 -0800

    Fixed app initialization options with iOS

--- Lib/eacp/Core/Threads/EventLoop-iOS.mm
+++ Lib/eacp/Core/Threads/EventLoop-iOS.mm
@@ -1,11 +1,38 @@
 #include "EventLoop.h"
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface EACPAppDelegate : UIResponder <UIApplicationDelegate>
+@property(strong, nonatomic) UIWindow* window;
+@end
+
+@implementation EACPAppDelegate
+- (BOOL)application:(UIApplication*)application
+    didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+{
+    return YES;
+}
+@end
 
 namespace eacp::Threads
 {
+static bool appInitialized = false;
+
 void EventLoop::run()
 {
-    CFRunLoopRun();
+    if (!appInitialized)
+    {
+        
+        appInitialized = true;
+        @autoreleasepool
+        {
+            UIApplicationMain(0, nil, nil, NSStringFromClass([EACPAppDelegate class]));
+        }
+    }
+    else
+    {
+        CFRunLoopRun();
+    }
 }
 
 void EventLoop::quit()
