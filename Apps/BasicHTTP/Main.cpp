0a88c173c2ba7adeb9475f247b2418cafb5a713c
Eyal Amir <eyalamirmusic@gmail.com>
2026-03-07 12:17:45 -0800

    Added BasicHttp example

---
+++ Apps/BasicHTTP/Main.cpp
@@ -0,0 +1,14 @@
+#include <eacp/Core/Core.h>
+#include <iostream>
+
+int main()
+{
+    auto req = eacp::HTTP::Request("https://echo.free.beeceptor.com");
+    req.type = "POST";
+    req.body = "Here's some body";
+
+    auto res = req.perform();
+    std::cout << res.content;
+
+    return 0;
+}
