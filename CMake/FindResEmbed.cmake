550435d6e4e855258dc01bd07155e4646e715c44
Eyal Amir <eyalamirmusic@gmail.com>
2026-07-11 23:10:10 -0700

    Added removing of transitive includes for libcpp and other cmake fixes

--- CMake/FindResEmbed.cmake
+++ CMake/FindResEmbed.cmake
@@ -1,7 +1,6 @@
+include(CPM)
+
 CPMAddPackage(
         NAME ResEmbed
         GITHUB_REPOSITORY eyalamirmusic/ResEmbed
         GIT_TAG main)
-
-# The runtime lib links into eacp plugins; keep its symbols out of their
-# export tables. ResourceGenerator is a host tool, not linked anywhere.
